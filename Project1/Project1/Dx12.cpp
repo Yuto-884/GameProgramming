7f66d3d8d5bfb96b63cdfe031af9f7e1b3068f7b
akio1230 <huangxinqiuwei@gmail.com>
2025-11-15 18:11:11 +0900

    1115
    1115

--- Project1/Project1/Dx12.cpp
+++ Project1/Project1/Dx12.cpp
@@ -173,3 +173,13 @@ void Dx12::EnableDebugLayer() {
 #endif
 }
 
+bool Dx12::create (const Device& device, D3D12_DESCRIPTOR_HEAP_TYPE type, UINT numDescriptors, bool shaderVisible) noexcept {
+    // RTVディスクリプタヒープの作成例
+    D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = {};
+    rtvHeapDesc.NumDescriptors = 2;  // スワップチェーンのバッファ数
+    rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+
+    ID3D12DescriptorHeap* rtvHeap;
+    device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
+}
